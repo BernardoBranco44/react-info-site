e24db548355cbcca8839451b924aa31e9cc93014
Bernardo Branco <115721031+BernardoBranco44@users.noreply.github.com>
2023-01-04 14:00:47 +0000

    updated main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,28 @@
-import React from "react"
+import React from "react";
 
 export default function Main() {
-    return <h1>Main component</h1>
+  return (
+    <div className="main">
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>
+          <span>Was first released in 2013</span>
+        </li>
+        <li>
+          <span>Was originally created by Jordan Walke</span>
+        </li>
+        <li>
+          <span>Has over 100K stars on GitHub</span>
+        </li>
+        <li>
+          <span>Is maintained by Facebook</span>
+        </li>
+        <li>
+          <span>
+            Powers thousands of enterprise apps, including mobile apps
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,12 @@
-import React from "react"
+import React from "react";
+import logo from "../images/react-icon-small.png";
 
 export default function Navbar() {
-    return <h1>Navbar component</h1>
+  return (
+    <div className="navbar">
+      <img src={logo} alt="react logo" />
+      <h1 id="title">ReactFacts</h1>
+      <h2>React Course - Project 1</h2>
+    </div>
+  );
 }
